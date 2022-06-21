379e118b2bd18f49cb617564a0eeb8122367c4c0
Moshiur Rahman <moshiur_cse@hotmail.com>
2022-06-21 10:12:52 +0600

    add Master Tables

---
+++ ESAQAApp/Models/Master/EsaMaster.cs
@@ -0,0 +1,16 @@
+﻿namespace BAC007.Models.Master
+{
+    public class EsaMaster
+    {
+        [Key]
+        public int MasterDataId { get; set; }
+
+        [StringLength(50)]
+        [Display(Name = "Name of the Respondent ")]
+        public string RespondentName { get; set; }
+
+        [StringLength(10)]
+        [Display(Name = "Date")]
+        public string FormDate { get; set; }
+    }
+}
